67e22923a733cb2a1f54d792c893a55a2bcbab4a
noilaibodullayeva <noilaibodullayeva07@gmail.com>
2023-11-28 20:30:35 -0800

    header

---
+++ src/header/index.js
@@ -0,0 +1,205 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Img1 from '../img/logoo.png'
+import { Input, InputAdornment, TextField } from '@mui/material';
+import { Search, SearchOffRounded } from '@mui/icons-material';
+
+const pages = ['Home', 'Menu', 'About', 'Products', 'Review', 'Contact', 'Blogs'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{backgroundColor:'#010103'}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'Roboto, sans-serif',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            fontSize: '30px'
+                        }}
+                    >
+                        Coffee
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                                width:'200px',
+                                left:'-20px',
+                                top:'5px'
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography sx={{
+                                        color: '#222',
+                                        "&:hover": {
+                                            borderBottom: '1px solid #d3ad7f',
+                                        },
+                                        textAlign:"center",
+                                        width:'300px',
+                                    }} >{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            fontFamily: 'Roboto, sans-serif',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            fontSize: '25px',
+                            textAlign:'center',
+                            marginRight:'40%'
+                        }}
+                    >
+                        Coffee
+                    </Typography>
+                    <Box sx={{
+                        flexGrow: 1,
+                        display: { xs: 'none', md: 'flex' },
+                        gap: '7px',
+                        justifyContent: 'center'
+                    }}>
+                        {pages.map((page) => (
+                            <Typography
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    margin: '0 1rem',
+                                    fontSize: '1rem',
+                                    color: '#fff',
+                                    display: 'block',
+                                    gap: '5px',
+                                    "&:hover": {
+                                        borderBottom: '.1rem solid #d3ad7f',
+                                        color: '#d3ad7f',
+                                    },
+
+                                }}
+                            >
+                                {page}
+                            </Typography>
+                        ))}
+                    </Box>
+                    <Box>
+                        <SearchOffRounded
+                            size="large"
+                            aria-label="account of current user"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                        />
+                        <Box sx={{
+                            position: 'absolute',
+                            top: '15%',
+                            right: '7%',
+                            background: '#fff',
+                            width: '50rem',
+                            height: '5rem',
+                            display: 'flex',
+                            alignItems: 'center',
+                            transform: 'scaleY(0)',
+                            transformOrigin: 'top',
+                        }}>
+
+                            <TextField id="standard-basic" label="search here..." variant="standard" startAdornment={
+                                <InputAdornment position="end">
+                                    <SearchOffRounded />
+                                </InputAdornment>
+                            }
+                                sx={{
+                                    height: '100 %',
+                                    width: '100 %',
+                                    fontSize: '1.6rem',
+                                    color: '#222',
+                                    padding: '1rem',
+                                    textTransform: 'none',
+                                }}
+                                onClose={handleCloseNavMenu} />
+                        </Box>
+
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
+
+
+const styles = {
+    img: {
+        width: '70px',
+        height: '70px',
+        borderRadius: '50%'
+    }
+}
